ed3d2d41ed365b877bab4e855cf9100244820557
Victoria Shaw <123508112+VShawFluenta@users.noreply.github.com>
2025-01-23 22:36:14 -0800

    About to move functions to a library file

---
+++ CameraFunctions.h
@@ -0,0 +1,11 @@
+
+
+
+void calibrateAccelerometer()
+void getDeviceInfo();
+void increaseFootageQuality(); 
+void stopRecording(); 
+void process_RunCam_data(); 
+
+bool process_Accelerometer_data_ADXL375(); 
+bool process_Accelerometer_data(); 
