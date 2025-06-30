9d1569db974fa4591dea684400844b1bb43a528b
Khephren Gould <154032288+KhephG1@users.noreply.github.com>
2025-06-30 15:31:33 -0500

    calibrate accelerometer properly, set proper limit of 9.33 g based on RASAERO sims

--- source/flight_code/src/accelerometer_filter.h
+++ source/flight_code/src/accelerometer_filter.h
@@ -5,25 +5,21 @@
 #include <Arduino.h>
 #ifndef ACCELEROMETER_FILTER_H
 #define ACCELEROMETER_FILTER_H
-template <uint8_t N, class input_t = uint16_t, class sum_t = uint32_t>
+template <uint8_t N, class input_t = float, class sum_t = float>
 class accelerometer_SMA {
 
 public:
-  input_t operator()(input_t input) {
+  sum_t operator()(input_t input) {
     input /= 2048; // converts raw adc to gs
-    // don't need subtract 1 g due to calibration
+
     sum -= previousInputs[index];
     sum += input;
     previousInputs[index] = input;
     if (++index == N)
       index = 0;
-    return (sum + (N / 2)) / N;
+    return (sum) / N;
   }
 
-  static_assert(
-      sum_t(0) < sum_t(-1), // check that sum_t is an undefined type
-      "Error sum data type should be an unsinged integer."
-      "Otherwise, the rounding operation in the return statement is invalid");
 
 private:
   uint8_t index{};
